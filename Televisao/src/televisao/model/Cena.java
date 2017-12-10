0732e97c80468a012de1645aec70decdf438901d
Luis <cunhaluisg5@gmail.com>
2017-12-10 19:55:15 -0200

    Get e Set classe Cena

--- Televisao/src/televisao/model/Cena.java
+++ Televisao/src/televisao/model/Cena.java
@@ -2,5 +2,29 @@
 package televisao.model;
 
 public class Cena {
-    
+    private String nome;
+    private Producao producao;
+
+    public Cena() {
+    }
+
+    public Cena(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Producao getProducao() {
+        return producao;
+    }
+
+    public void setProducao(Producao producao) {
+        this.producao = producao;
+    }
 }
