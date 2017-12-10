5325350100faa94cfbc1c9a81257572e2e7d7fd5
Luis <cunhaluisg5@gmail.com>
2017-12-10 20:02:17 -0200

    Get e set da classe Fita

--- Televisao/src/televisao/model/Fita.java
+++ Televisao/src/televisao/model/Fita.java
@@ -2,5 +2,31 @@
 package televisao.model;
 
 public class Fita {
-    
+    private String numeroSerie;
+    private int capacidade;
+
+    public Fita() {
+        capacidade = 120;
+    }
+
+    public Fita(String numeroSerie, int capacidade) {
+        this.numeroSerie = numeroSerie;
+        this.capacidade = capacidade;
+    }
+
+    public String getNumeroSerie() {
+        return numeroSerie;
+    }
+
+    public void setNumeroSerie(String numeroSerie) {
+        this.numeroSerie = numeroSerie;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
 }
