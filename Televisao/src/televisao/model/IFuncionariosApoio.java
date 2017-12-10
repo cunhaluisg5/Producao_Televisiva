e45800ebb1b5cb064f575c9b984cc8050c55ee00
Luis <cunhaluisg5@gmail.com>
2017-12-10 20:07:03 -0200

    Implementação da interface IFuncionariosApoio

--- Televisao/src/televisao/model/IFuncionariosApoio.java
+++ Televisao/src/televisao/model/IFuncionariosApoio.java
@@ -2,5 +2,8 @@
 package televisao.model;
 
 public interface IFuncionariosApoio {
-    
+    public static double SALARIO_CAMERA = 3000;
+    public static double SALARIO_CONTRARREGRA = 3000;
+    public static double SALARIO_ILUMINADOR = 3000;
+    public double valorSalario();
 }
