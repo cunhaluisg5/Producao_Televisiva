c362988b52880c949dc54ecd382c77526988034d
Luis <cunhaluisg5@gmail.com>
2017-12-10 20:11:31 -0200

    Get e Set da classe Iluminador

--- Televisao/src/televisao/model/Iluminador.java
+++ Televisao/src/televisao/model/Iluminador.java
@@ -2,5 +2,16 @@
 package televisao.model;
 
 public class Iluminador{
-    
+    private Producao producao;
+
+    public Iluminador() {
+    }
+
+    public Producao getProducao() {
+        return producao;
+    }
+
+    public void setProducao(Producao producao) {
+        this.producao = producao;
+    }
 }
