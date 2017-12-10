6f397fd8b01eed290b379c65932398d7603ea228
Luis <cunhaluisg5@gmail.com>
2017-12-10 20:19:34 -0200

    Métodos implementados na classe Camera

--- Televisao/src/televisao/model/Camera.java
+++ Televisao/src/televisao/model/Camera.java
@@ -1,17 +1,30 @@
 
 package televisao.model;
 
-public class Camera{
+public class Camera extends Funcionario implements IFuncionariosApoio{
     private Producao producao;
 
     public Camera() {
     }
-    
+
+    public Camera(String nome, int idade) {
+        super(nome, idade);
+    }
+
     public Producao getProducao() {
         return producao;
     }
 
     public void setProducao(Producao producao) {
         this.producao = producao;
+    }
+
+    public double val() {
+        return SALARIO_CAMERA;
+    }
+    
+    @Override
+    public double valorSalario() {
+        return producao.getTempo() * SALARIO_CAMERA;
     }
 }
