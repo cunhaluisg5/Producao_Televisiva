b256cbf4f9ccc7402c7bb418ea8a78981f1b15d0
Luis <cunhaluisg5@gmail.com>
2017-12-10 19:52:54 -0200

    Get e Set da classe Camera

--- Televisao/src/televisao/model/Camera.java
+++ Televisao/src/televisao/model/Camera.java
@@ -2,5 +2,16 @@
 package televisao.model;
 
 public class Camera{
+    private Producao producao;
+
+    public Camera() {
+    }
     
+    public Producao getProducao() {
+        return producao;
+    }
+
+    public void setProducao(Producao producao) {
+        this.producao = producao;
+    }
 }
