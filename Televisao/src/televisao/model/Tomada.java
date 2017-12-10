1db6332a6e80f36ba7c4457e311e5355f4958810
Luis <cunhaluisg5@gmail.com>
2017-12-10 20:15:40 -0200

    Get e Set da classe Tomada

--- Televisao/src/televisao/model/Tomada.java
+++ Televisao/src/televisao/model/Tomada.java
@@ -2,5 +2,48 @@
 package televisao.model;
 
 public class Tomada {
-    
+    private String nome;
+    private int duracao;
+    private Cena cena;
+    private int val;
+
+    public Tomada() {
+    }
+
+    public Tomada(String nome, int duracao) {
+        this.nome = nome;
+        this.duracao = duracao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = (nome + val);
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public Cena getCena() {
+        return cena;
+    }
+
+    public void setCena(Cena cena) {
+        this.cena = cena;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
 }
