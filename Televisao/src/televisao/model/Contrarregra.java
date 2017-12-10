7213b06963c9b21cc6f78ce918dfda185f124e29
Luis <cunhaluisg5@gmail.com>
2017-12-10 19:57:39 -0200

    Get e Set da classe Contrarregra

--- Televisao/src/televisao/model/Contrarregra.java
+++ Televisao/src/televisao/model/Contrarregra.java
@@ -1,6 +1,16 @@
 
 package televisao.model;
-
 public class Contrarregra{
-    
+    private Producao producao;
+
+    public Contrarregra() {
+    }
+
+    public Producao getProducao() {
+        return producao;
+    }
+
+    public void setProducao(Producao producao) {
+        this.producao = producao;
+    }
 }
