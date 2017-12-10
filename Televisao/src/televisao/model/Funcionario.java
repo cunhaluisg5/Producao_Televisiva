57b20101d885d345a8319ff158395ad3a254be78
Luis <cunhaluisg5@gmail.com>
2017-12-10 20:04:13 -0200

    Get e Set da classe Funcionario

--- Televisao/src/televisao/model/Funcionario.java
+++ Televisao/src/televisao/model/Funcionario.java
@@ -2,5 +2,39 @@
 package televisao.model;
 
 public class Funcionario{
-    
+    private String nome;
+    private int idade;
+    private boolean ehAtor = false;
+
+    public Funcionario() {
+    }
+
+    public Funcionario(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public boolean isEhAtor() {
+        return ehAtor;
+    }
+
+    public void setEhAtor(boolean ehAtor) {
+        this.ehAtor = ehAtor;
+    }
 }
