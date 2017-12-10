ce19679ab410bfc44d2577cc2e85c5ce44c4c8ff
Luis <cunhaluisg5@gmail.com>
2017-12-10 19:50:14 -0200

    Get e Set da classe Ator

--- Televisao/src/televisao/model/Ator.java
+++ Televisao/src/televisao/model/Ator.java
@@ -2,5 +2,29 @@
 package televisao.model;
 
 public class Ator{
-    
+    private double salario;
+    private Producao producao;
+
+    public Ator() {
+    }
+
+    public Ator(double salario) {
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public Producao getProducao() {
+        return producao;
+    }
+
+    public void setProducao(Producao producao) {
+        this.producao = producao;
+    }
 }
