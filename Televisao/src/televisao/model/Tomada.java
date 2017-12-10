b003fd6356936aa857567ec21774b347d72a6016
Luis <cunhaluisg5@gmail.com>
2017-12-10 20:51:34 -0200

    Métodos adicionados na classe Tomada

--- Televisao/src/televisao/model/Tomada.java
+++ Televisao/src/televisao/model/Tomada.java
@@ -1,18 +1,24 @@
 
 package televisao.model;
 
+import java.text.NumberFormat;
+import java.util.ArrayList;
+
 public class Tomada {
     private String nome;
     private int duracao;
     private Cena cena;
     private int val;
+    private ArrayList<Fita> fitas;
 
     public Tomada() {
+        this. fitas = new ArrayList<>();
     }
 
     public Tomada(String nome, int duracao) {
         this.nome = nome;
         this.duracao = duracao;
+        this. fitas = new ArrayList<>();
     }
 
     public String getNome() {
@@ -39,11 +45,34 @@ public class Tomada {
         this.cena = cena;
     }
 
+    public ArrayList<Fita> getFitas() {
+        return fitas;
+    }
+
+    public void setFitas(ArrayList<Fita> fitas) {
+        this.fitas = fitas;
+    }
+
     public int getVal() {
         return val;
     }
 
     public void setVal(int val) {
         this.val = val;
+    }
+    
+    public void adicionaFitas(int n){
+        Fita fita = new Fita();
+        fitas.add(fita);
+        String recebe = NumberFormat.getInstance().format(getFitas().size());
+        fita.setNumeroSerie("#" + n + "." + recebe);
+        int val = duracao - fita.getCapacidade();
+        while(val > 0){
+            Fita fita2 = new Fita();
+            fitas.add(fita2);
+            recebe = NumberFormat.getInstance().format(getFitas().size());
+            fita2.setNumeroSerie("#" + n + "." + recebe);
+            val -= fita.getCapacidade();
+        }
     }
 }
