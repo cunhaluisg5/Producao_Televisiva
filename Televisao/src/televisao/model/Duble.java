5a8d57818979105831c48d1d4d07114efbca0ba4
Luis <cunhaluisg5@gmail.com>
2017-12-10 20:00:03 -0200

    Get e Set classe Duble

--- Televisao/src/televisao/model/Duble.java
+++ Televisao/src/televisao/model/Duble.java
@@ -3,4 +3,38 @@ package televisao.model;
 
 public class Duble{
     
+    private Ator ator;
+    private double salario;
+    private Producao producao;
+
+    public Duble() {
+    }
+
+    public Duble(double salario) {
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public Ator getAtor() {
+        return ator;
+    }
+
+    public void setAtor(Ator ator) {
+        this.ator = ator;
+    }
+
+    public Producao getProducao() {
+        return producao;
+    }
+
+    public void setProducao(Producao producao) {
+        this.producao = producao;
+    }
 }
